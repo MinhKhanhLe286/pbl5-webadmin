974dc60e28472a4000ccee3ba43b7d0e4a8932f3
Lê Minh Khánh <162238732+MinhKhanhLe286@users.noreply.github.com>
2024-11-09 13:21:30 +0700

    test io.sockets.emit

--- src/server.js
+++ src/server.js
@@ -1,45 +1,29 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
 const http = require('http');
-const socketIo = require('socket.io');
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: 'http://127.0.0.1:5500',  
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  }
-});
+const app = express();
+var server = http.Server(app);
+var io = require('socket.io')(server);
 
-var temperaturevalue = 0;
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', "./src/views");
 
-setInterval(() => {
-  temperaturevalue = Math.random() * 100; 
-  // console.log(temperaturevalue); 
-  // // Gửi giá trị nhiệt độ mới đến tất cả client đang kết nối
-  // io.emit('temperature', temperaturevalue);
-}, 1000);
-
-// Đảm bảo phục vụ file HTML từ server
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Cung cấp file index.html
-});
-
-// Khi có kết nối
-io.on('connection', (socket) => {
-  console.log('Có người kết nối');
-  
-  // Khi kết nối, ngay lập tức gửi giá trị nhiệt độ hiện tại
-  socket.emit('temperature', temperaturevalue);
-
-  socket.on('disconnect', () => {
-    console.log('Người dùng đã ngắt kết nối');
-  });
-});
-
-// Lắng nghe ở cổng 3333
-server.listen(3333, () => {
+server.listen(3333, ()=>{
   console.log('Server is running at http://localhost:3333');
 });
+
+app.get("/", (req, res) => {
+  res.render("trangchu");
+});
+
+// tạo socket kết nối với sêrver
+io.on("connection", (socket)=>{
+  console.log(`Có người kết nối ${socket.id}`)
+  socket.on("disconnect", () => {
+    console.log(`Có người đã hủy kết nối id như sau: ${socket.id}`);
+  });
+  socket.on("Client-send-color", (data)=>{
+    console.log("Có người nhấn nút  "+data);
+    io.sockets.emit("Server-response-color", data);
+  })
+})
